f4376dc04385a7ee88d3afda0d0bcb812adba5eb
Mary Theofield <mptheofield@gmail.com>
2022-11-06 20:12:50 -0500

    started the backend for project 3

---
+++ Projectmanagementapp/server/sampleData.js
@@ -0,0 +1,85 @@
+//Projects 
+const projects = [
+    {
+        id: '1',
+        clientId: '1',
+        name: 'weather app',
+        description: 'Lorem20',
+        status: 'end progress',
+
+    },
+    {
+        id: '2',
+        clientId: '2',
+        name: 'movie app',
+        description: 'favorite',
+        status: 'done',
+
+    },
+
+    {
+        id: '3',
+        clientId: '3',
+        name: 'portfolio app',
+        description: 'test',
+        status: 'finished',
+
+    },
+    {
+        id: '4',
+        clientId: '4',
+        name: 'music app',
+        description: 'music',
+        status: 'end progress',
+
+    },
+    {
+        id: '5',
+        clientId: '5',
+        name: 'book app',
+        description: 'Lorem20',
+        status: 'end progress',
+
+    },
+]
+
+    // client
+    const clients = [
+        {
+            id: '1',
+            name: 'John',
+            email: 'ironman@gmail.com',
+            phone: '123-3242',
+        
+        },
+        {
+            id: '2',
+            name: 'Amy',
+            email: 'batman@gmail.com',
+            phone: '132-3242',
+        
+        },
+        {
+            id: '3',
+            name: 'Sam',
+            email: 'sam@gmail.com',
+            phone: '123-2222',
+        
+        },
+        {
+            id: '4',
+            name: 'Mike',
+            email: 'mike@gmail.com',
+            phone: '123-3242',
+        
+        },
+        {
+            id: '5',
+            name: 'Ashley',
+            email: 'ashley@gmail.com',
+            phone: '333-3333',
+        
+        },
+    ];
+
+    module.exports = { projects, clients};
