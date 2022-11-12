ce44194bcb8bed8a6aa7ae3d674b11ad4ea40e41
Mary Theofield <mptheofield@gmail.com>
2022-11-12 07:20:18 -0500

    deugging to get database to work on graphql

--- projectapp/server/schema/schema.js
+++ projectapp/server/schema/schema.js
@@ -3,7 +3,7 @@ const { projects, clients } = require('../sampleData.js');
 const Project = require('../models/Project');
 const Client = require('../models/Client');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull, } = require('graphql');
 // Project Type
 const ProjectType = new GraphQLObjectType({
     name: 'Project',
@@ -64,6 +64,27 @@ const RootQuery = new GraphQLObjectType({
     },
 });
 
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: {type: {GraphQLNonNull(GraphQLString)},
+                    email: {type: GraphQLNonNull(GraphQLString)},
+                    phone: {type: GraphQLNonNull(GraphQLString)},
+        }, 
+        resolve(parent, args) {
+            const client = new Client({
+                name: args.name, 
+                email: args.email,
+                phone: args.phone,
+            });
+            return client.save();
+            
+        }
+    }
+})
 module.exports = new GraphQLSchema({
     query:RootQuery
 })
