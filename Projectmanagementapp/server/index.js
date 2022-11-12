2a8e2f08c5c285a899a67f722b55ad4ab5e0977c
Mary Theofield <mptheofield@gmail.com>
2022-11-11 20:39:28 -0500

    debugged to get npm run dev

--- Projectmanagementapp/server/index.js
+++ Projectmanagementapp/server/index.js
@@ -4,15 +4,14 @@ const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 6000;
 
 const app = express();
 
 // Connect to database
-connectDB();
 
-app.use(cors());
+
 
 app.use(
   '/graphql',
--- Projectmanagementapp/server/schema/schema.js
+++ Projectmanagementapp/server/schema/schema.js
@@ -1,4 +1,4 @@
-const { projects, clients } = require('../sampleData.js')
+const { projects, clients } = require('../sampleData.js');
 
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema} = require('graphql');
 
@@ -6,23 +6,24 @@ const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema} = require('g
 const ClientType = new GraphQLObjectType({
     name: 'Client',
     fields: () => ({
-        id: { type: GraphQLID},
-        name: { type: GraphQLString},
-        email: { type: GraphQLString},
-        phone: { type: GraphQLString}
-    })
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    }),
 });
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        client: {
         type: ClientType,
         args: {id: { type: GraphQLID}},
         resolve(parent, args) {
             return clients.find(client => client.id === args.id)
         }
     },
-});
+}});
 
 module.exports = new GraphQLSchema({
     query:RootQuery
