b836793951cd32ed0e65d00b2040ae3a10853880
Mary Theofield <mptheofield@gmail.com>
2022-11-12 06:48:06 -0500

    connecting client.js and project.js to schema.js

--- projectapp/server/schema/schema.js
+++ projectapp/server/schema/schema.js
@@ -1,4 +1,7 @@
-const { projects, clients } = require('../sampleData.js')
+const { projects, clients } = require('../sampleData.js');
+
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
 // Project Type
@@ -12,7 +15,7 @@ const ProjectType = new GraphQLObjectType({
         client: {
             type: ClientType,
             resolve(parent, args) {
-            return clients.find(client => client.id === parent.clientId);
+            return clients.findById(parent.clientId);
         },
     },
     })
@@ -35,27 +38,27 @@ const RootQuery = new GraphQLObjectType({
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
-                return projects;
+                return Project.find();
             }
         },
         project: {
          type: ProjectType,
          args: { id: { type: GraphQLID } },
          resolve(parent, args) {
-             return projects.find(project => project.id === args.id);
+             return Project.findById(args.id);
          },
         },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
-                return clients;
+                return clients.find();
             }
         },
         client: {
          type: ClientType,
          args: { id: { type: GraphQLID } },
          resolve(parent, args) {
-             return clients.find(client => client.id === args.id);
+             return clients.find(args.id);
          },
         },
     },
