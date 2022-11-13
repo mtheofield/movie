14678597e7d9c943cfdd8fec3724c30fe9489875
Mary Theofield <mptheofield@gmail.com>
2022-11-13 12:42:37 -0500

    working to ad client queries

--- projectapp/client/src/components/Clients.jsx
+++ projectapp/client/src/components/Clients.jsx
@@ -1,14 +1,6 @@
-import {gql, useQuery} from '@apollo/client'
+import {gql, useQuery} from '@apollo/client';
+import CLientRow from "./ClientRow";
 
-const GET_CLIENTS = gql`
-    query getClients {
-        clients {
-            id
-            name
-            email
-            phone
-        }
-    }`
 
 export default function Clients() {
     const { loading, error, data} = useQuery(GET_CLIENTS)
@@ -16,6 +8,21 @@ export default function Clients() {
     if (loading) return <p> loading...</p>
     if (error) return <p> something went wrong </p> 
     
-  return <>{!loading && !error && <h1>Clients</h1>}</>;
+  return <>{!loading && !error && (
+    <table className= 'table table-hover mt-3'> 
+    <thead> 
+        <tr>
+        <th>name</th>
+        <th>email</th>
+        <th>phone</th>
+        </tr>
+        </thead>
+        <tbody>
+            {data.clients.map(client =>(
+                <CLientRow key={client.id} client={client} />
+            ))}
+        </tbody>
+        </table>
+   )} </>;
     
 }
