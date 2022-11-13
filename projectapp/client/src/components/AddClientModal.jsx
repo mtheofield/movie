5c79f0f267b89a2181baa610d17c9a88e9ad7bb2
Mary Theofield <mptheofield@gmail.com>
2022-11-13 13:23:31 -0500

    client Modal

---
+++ projectapp/client/src/components/AddClientModal.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import { FaUser } from 'react-icons/fa';
+import {useMutation} from '@apollo/client';
+ 
+export default function AddClientModal() {
+  return (
+    <div>AddClientModal</div>
+  )
+}
