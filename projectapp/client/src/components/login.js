0468d68a0b9e8424d14892d616d75b7beb14fe1d
Mary Theofield <mptheofield@gmail.com>
2022-11-17 03:25:44 -0500

    debugging code more

--- projectapp/client/src/components/login.js
+++ projectapp/client/src/components/login.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 import PropTypes from 'prop-types';
 
 async function loginUser(credentials) {
-  return fetch("http://localhost:3000/login",
+  return fetch("http://localhost:3001/login",
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
--- projectapp/server/config/db.js
+++ projectapp/server/config/db.js
@@ -1,27 +1,29 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+// const connectDB = async () => {
+//   const conn = await mongoose.connect(process.env.MONGO_URI);
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-};
+//   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
 
 // const mongoose = require('mongoose');
-// const root = require('app-root-path');
-// const path = require('path');
+const mongoose = require('mongoose');
+const root = require('app-root-path');
+const path = require('path');
 
-// require('dotenv').config({path:root +path.sep + ".env"});
+require('dotenv').config({path:root +path.sep + ".env"});
 
-// mongoose.connect(
-//     process.env.MONGODB_URI || 'mongodb://localhost/destination_app',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   }
-// );
-// //process.env.MONGODB_URI ||
-// module.exports = mongoose.connection;
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/travel_blog_db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+//process.env.MONGODB_URI ||
+module.exports = mongoose.connection;
+
