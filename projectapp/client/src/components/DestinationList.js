884bf38866413e636b2996d79603a7931359fca6
Mary Theofield <mptheofield@gmail.com>
2022-11-17 01:57:34 -0500

    using REACT allowing user to shaire their destination and whhere they have been

--- projectapp/client/src/components/DestinationList.js
+++ projectapp/client/src/components/DestinationList.js
@@ -1,99 +1,36 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-import axios from 'axios'
-
-class Signup extends Component {
-	constructor() {
-		super()
-		this.state = {
-			username: '',
-			password: '',
-			confirmPassword: '',
-			redirectTo: null
-
-		}
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-	}
-	handleChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-	}
-	handleSubmit(event) {
-		console.log('sign-up handleSubmit, username: ')
-		console.log(this.state.username)
-		event.preventDefault()
+import React from 'react';
 
 
-		axios.post('/user/', {
-			username: this.state.username,
-			password: this.state.password
-		})
-		// alert('test')
-			.then(response => {
-				console.log(response)
-				if (!response.data.errmsg) {
-					console.log('welcome you are now signed up!')
-					this.setState({redirectTo: '/login'
-					})
-				} else {
-					console.log('sorry this username exits')
-				}
-			}).catch(error => {
-				console.log('there was an error: ')
-				console.log(error)
+const DestinationList = (props) => {
 
-			})
+	if (!props.destinations.length) {
+		return <h3>{props.message} No destination entered</h3>;
 	}
 
-
-	render() {
-		if (this.state.redirectTo) {
-			return <Redirect to={{ pathname: this.state.redirectTo }} />
-		} else {
-			return (
-				<div className="SignUpContainer">
-					<div className="SignupForm">
-						<h4>Sign up</h4>
-						<form>
-							<div className="form-group">
-								<label className="form-label" htmlFor="username"></label>
-								<input className="form-input"
-									type="text"
-									id="username"
-									name="username"
-									placeholder="Username"
-									value={this.state.username}
-									onChange={this.handleChange}
-								/>
+	return (
+		<div className="">
+			<h3>{props.title}</h3>
+			{props.destinations &&
+				props.destinations.map((destination) => (
+					<div key={destination._id} className="mb-3 destinationCard">
+						<h3 className=" p-2 m-0 card-header">
+							{destination.destinationTitle} <br />
+							<span style={{ fontSize: '0.8rem' }}>
+								{destination.createdAt}
+							</span>
+						</h3>
+						<div className="  p-2">
+							<p>{destination.description}</p>
+						</div>
+						<div className="  p-2">
+							<div className="polaroid-container">
+								<img src={destination.imageUrl} className="pure-img polaroid rotate10" />
 							</div>
-
-							<div className="form-group">
-								<label className="form-label" htmlFor="password"> </label>
-								<input className="form-input"
-									placeholder="Password"
-									type="password"
-									name="password"
-									value={this.state.password}
-									onChange={this.handleChange}
-								/>
-							</div>
-
-							<div className="form-group ">
-								<button
-									className="btn btn-primary col-mr-auto"
-									onClick={this.handleSubmit}
-									type="submit"
-								>Submit</button>
-							</div>
-						</form>
+						</div>
 					</div>
-				</div>
+				))}
+		</div>
+	);
+};
 
-			)
-		}
-	}
-}
-
-export default Signup
+export default DestinationList;
