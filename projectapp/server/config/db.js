13a4b7401eaefa959844766363d6347ffa6a5e01
Mary Theofield <mptheofield@gmail.com>
2022-11-17 13:57:39 -0500

    debugged to get form to work

--- projectapp/server/config/db.js
+++ projectapp/server/config/db.js
@@ -1,39 +1,14 @@
-// const mongoose = require('mongoose');
-
-//   const conn = await mongoose.connect(process.env.MONGO_URI) {
-//     useNew
-// }
-
-//   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-// };
-
-// module.exports = connectDB;
-
-// const mongoose = require('mongoose');
-// const mongoose = require('mongoose');
-// const root = require('app-root-path');
-// const path = require('path');
-
-// require('dotenv').config({path:root +path.sep + ".env"});
-
-// mongoose.connect(
-//     process.env.MONGODB_URI || 'mongodb://localhost/travel_blog_db',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   }
-// );
-// //process.env.MONGODB_URI ||
-// module.exports = mongoose.connection;
-
-
 const mongoose = require('mongoose');
+const root = require('app-root-path');
+const path = require('path');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/destination_app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+require('dotenv').config()
+mongoose.connect(
+    process.env.MONGO_URI || 'mongodb://localhost/destination_app',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
--- projectapp/server/server.js
+++ projectapp/server/server.js
@@ -48,3 +48,6 @@ db.once('open', () => {
   });
 });
 }
+
+
+startApolloServer(typeDefs, resolvers)
